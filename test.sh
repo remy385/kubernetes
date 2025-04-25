70611e70c9c8ad10f05aa596bb866481183138f6
remy385 <114920780+remy385@users.noreply.github.com>
2025-04-25 15:45:04 +0200

    adva,ce

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-kubectl apply -f ./secret.yaml
-kubectl apply -f ./configmap.yaml
+#kubectl apply -f ./secret.yaml
+#kubectl apply -f ./configmap.yaml
 kubectl apply -f ./deploy.yaml
 kubectl apply -f ./service.yaml
 kubectl apply -f ./ingress.yaml
