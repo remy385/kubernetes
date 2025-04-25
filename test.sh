8b73298c0926aece75dd22aff6f00f670b29ba1d
remy385 <114920780+remy385@users.noreply.github.com>
2025-04-25 14:22:57 +0200

    first commit

---
+++ test.sh
@@ -0,0 +1,6 @@
+kubectl apply -f ./secret.yaml
+kubectl apply -f ./configmap.yaml
+kubectl apply -f ./deploy.yaml
+kubectl apply -f ./service.yaml
+kubectl apply -f ./ingress.yaml
+kubectl apply -f ./autoscaler.yaml
